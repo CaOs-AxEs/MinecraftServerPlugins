f7926c15566b27c033af8245fa08d8b931ccb65c
CaOs-AxEs <06123109jb@gmail.com>
2022-07-05 20:30:32 +0100

    Initial Commit

---
+++ src/main/java/TestPlugin/TestPlugin.java
@@ -0,0 +1,24 @@
+package TestPlugin;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.*;
+import org.spigotmc.*;
+
+public final class TestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        for(Player player : getServer().getOnlinePlayers()){
+            player.sendTitle("Hello!", "Plugin Enabled.", 1, 3, 1);
+        }
+
+    }
+
+    @Override
+    public void onDisable() {
+        for(Player player : getServer().getOnlinePlayers()){
+            player.sendTitle("Bye!", "Plugin Disabling.", 1, 3, 1);
+        }
+    }
+}
